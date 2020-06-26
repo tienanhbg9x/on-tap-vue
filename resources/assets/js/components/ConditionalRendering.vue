de458f703c7cfb2eee3d12ca4c8fdcae3061276e
TienAnh6789 <anhht@watermelon.vn>
2020-06-26 17:12:25 +0700

    update

---
+++ resources/assets/js/components/ConditionalRendering.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="conditional-rendering">
+        <div class="block-1" v-if="currentBlock == 1">
+            This is block 1
+        </div>
+        <div class="block-2" v-else-if="currentBlock == 2">
+            This is block 2
+        </div>
+        <div class="block-3" v-else>
+            This is block 3
+        </div>
+        <div>
+            <button @click="showBlock(1)">Button 1</button>
+        </div>
+        <div>
+            <button @click="showBlock(2)">Button 2</button>
+        </div>
+        <div>
+            <button @click="showBlock(3)">Button 3</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ConditionalRendering",
+        data() {
+            return {
+                currentBlock: 1
+            }
+        },
+        methods: {
+            showBlock(number) {
+                this.currentBlock = number
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
